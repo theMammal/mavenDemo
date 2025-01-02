5681bb72fe9a4dcea3543be05f5c5486601502b5
theMammal <38924805+theMammal@users.noreply.github.com>
2025-01-02 14:11:44 -0500

    demo script updated jan 2

--- src/server/script.js
+++ src/server/script.js
@@ -4,5 +4,5 @@ export function showStateUpdate(current, previous) {
     const currentState = current.getValue('state')
     const previousState = previous.getValue('state')
 
-    gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`)
+    gs.addInfoMessage(`state has been updated from "${previousState}" to "${currentState}"`)
 }
